e6903d47d1db5fb84a4c6cdcdf573bcf5313cd93
gracielo2804 <56956599+gracielo2804@users.noreply.github.com>
2020-12-13 21:40:38 +0700

    Menu Awal

---
+++ Assets/Assets Temp/ScriptSceneMenu/CanvasScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasScript : MonoBehaviour
+{
+    
+    public GameObject CanvasMenu;
+    public GameObject PanelAwal;
+
+     private void Start() {
+        CanvasMenu.SetActive(true);
+        PanelAwal.SetActive(true);
+    }
+    public void play(){
+        Debug.Log("Masuk");
+        SceneManager.LoadScene("Stage 1");
+    }
+    public void exitGame(){
+        Application.Quit();
+    }
+}
